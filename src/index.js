3c5893dd000d29bb2be3c14ce500e06ab522a0f8
Enoh <eno.col@gmail.com>
2023-08-22 22:52:34 +0100

    Add gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './styles.css';
 
 function component() {
   const element = document.createElement('div');
   //lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
