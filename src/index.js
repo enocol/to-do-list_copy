6b1c71b1970ac72df1b096165c2bbd51f5e65994
Enoh <eno.col@gmail.com>
2023-08-23 09:33:44 +0100

    Edit index.html, index.js and styles.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import _ from 'lodash';
 import './styles.css';
 
-function component() {
-  const element = document.createElement('div');
-  // lodash, now imported by this script
-  element.innerHTML = _.join(['hello000', 'webpack'], ' ');
-  element.classList.add('hello');
+const toDoList = [
+  {
+    description: 'Read flexbox',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Code css grid',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'go to shop',
+    completed: false,
+    index: 3,
+  },
+];
 
-  return element;
+function renderTasks(todos) {
+  const todo = document.querySelector('.to-do-list');
+  const div2 = document.createElement('div');
+  div2.className = 'button-container';
+  const button = document.createElement('button');
+
+  todos.forEach((task) => {
+    const div1 = document.createElement('div');
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const div3 = document.createElement('div');
+    div3.className = 'icon';
+    div3.innerHTML = `<i class="fa-solid fa-ellipsis-vertical"></i>`;
+    checkbox.type = 'checkbox';
+    const div = document.createElement('div');
+    button.className = 'button';
+    button.innerText = 'Clear all completed';
+    div.className = 'task';
+    div1.className = 'taskcontainer';
+
+    listItem.textContent = task.description;
+    listItem.className = task.completed ? 'completed' : 'incomplete';
+    div.append(checkbox, listItem);
+    div1.append(div, div3);
+    todo.append(div1);
+  });
+  div2.append(button);
+  todo.append(div2);
 }
 
-document.body.append(component());
+renderTasks(toDoList);
