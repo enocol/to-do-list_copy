671d2a542bc85ca9e489eb4e70b031ba55ead271
Enoh <eno.col@gmail.com>
2023-08-25 11:15:19 +0100

    Impliment and edit delete function

--- src/deleteItem.js
+++ src/deleteItem.js
@@ -1,8 +1,10 @@
 import { updateDisplay } from './updateDisplay';
+import { updateIndices } from './updateIndices';
 
 export const deleteItem = (index) => {
   const data = JSON.parse(localStorage.getItem('data')) || [];
   data.splice(index, 1);
   localStorage.setItem('data', JSON.stringify(data));
   updateDisplay();
+  updateIndices();
 };
