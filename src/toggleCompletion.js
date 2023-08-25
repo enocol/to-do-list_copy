1241d94a978f97cd2b482ce2a18b885be08a087f
Enoh <eno.col@gmail.com>
2023-08-25 14:37:50 +0100

    Remove commented code

--- src/toggleCompletion.js
+++ src/toggleCompletion.js
@@ -1,10 +1,1 @@
-// import updateDisplay from './updateDisplay.js';
 
-// const toggleCompletion = (index) => {
-//   const data = JSON.parse(localStorage.getItem('data')) || [];
-//   data[index].completed = !data[index].completed;
-//   localStorage.setItem('data', JSON.stringify(data));
-//   updateDisplay();
-// };
-
-// export default toggleCompletion;
