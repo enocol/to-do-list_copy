07e4214ed5b6128b3af2371b86da550680fd8472
Enoh <eno.col@gmail.com>
2023-08-25 11:16:21 +0100

    Edit updatedisplay function

--- src/updateDisplay.js
+++ src/updateDisplay.js
@@ -1,12 +1,11 @@
 import { toggleCompletion } from './toggleCompletion';
 import { deleteItem } from './deleteItem';
+import { addToStorage } from './addToStogage';
 
 export const updateDisplay = () => {
   const data = JSON.parse(localStorage.getItem('data')) || [];
   const listTodo = document.querySelector('.to-do-list');
   listTodo.innerHTML = '';
-
-  console.log('data', data);
 
   data.forEach((todo, index) => {
     const listItem = document.createElement('li');
@@ -21,14 +20,30 @@ export const updateDisplay = () => {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = todo.completed;
+    checkbox.id = `${index}`;
     checkbox.addEventListener('change', () => {
       toggleCompletion(index);
     });
 
-    const textSpan = document.createElement('span');
-    textSpan.textContent = `${todo.index}. ${todo.description}`;
+    const inputBox = document.createElement('input');
+    inputBox.value = `${todo.description}`;
+    inputBox.setAttribute('readOnly', 'true');
+    inputBox.className = 'editable';
+    inputBox.classList.add('noneEdit');
 
-    listItem.append(checkbox, textSpan);
+    inputBox.addEventListener('focus', (event) => {
+      const target = event.target;
+      target.removeAttribute('readonly');
+    });
+
+    inputBox.addEventListener('blur', (event) => {
+      const target = event.target;
+      target.setAttribute('readonly', true);
+      data[index].description = target.value;
+      addToStorage(data);
+    });
+
+    listItem.append(checkbox, inputBox);
     container.append(listItem, iconDiv);
 
     listTodo.appendChild(container);
